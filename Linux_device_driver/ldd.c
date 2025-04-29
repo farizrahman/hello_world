f2ada68119de4c236e7ea7fbf1e46cc5e729fbc1
fariz Rahman T <37476994+farizrahman@users.noreply.github.com>
2025-04-29 12:02:08 +0530

    Add files via upload

---
+++ Linux_device_driver/ldd.c
@@ -0,0 +1,27 @@
+/*********************************************************************************************************
+*
+*	FileName			:	ldd.c
+*	Description			:	Dynamic lodable linux kernel module for printing hello world in kernel log
+*	Author				:	Fariz Rahman T
+*
+**********************************************************************************************************/
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");  //*
+MODULE_AUTHOR("Fariz");
+MODULE_DESCRIPTION("Hello world dynamic lodable kernel module");
+
+static int myModule_module_init(void)
+{
+  printk("Hello world (from myModule driver!!)\n");
+  return 0;
+}
+
+static void myModule_module_exit(void)
+{
+  printk("Good Bye (from myModule driver!!)\n");
+}
+
+module_init(myModule_module_init);
+module_exit(myModule_module_exit);
