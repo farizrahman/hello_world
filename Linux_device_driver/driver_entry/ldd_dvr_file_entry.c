72cc96aa77365db6d8db34e6209bca525e4a9e78
fariz Rahman T <37476994+farizrahman@users.noreply.github.com>
2025-05-01 11:07:39 +0530

    Create ldd_dvr_file_entry.c

---
+++ Linux_device_driver/driver_entry/ldd_dvr_file_entry.c
@@ -0,0 +1,35 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Fariz Rahman T");
+MODULE_DESCRIPTION("Ldd driver file entry creation");
+
+static struct proc_dir_entry *proc_dir_entry_pyjama = NULL;
+static struct proc_ops proc_ops_pyjama = {};
+
+static int ldd_driver_module_init(void)
+{
+    printk("ldd driver init entry");
+
+    proc_dir_entry_pyjama = proc_create("Pyjama_driver", 
+                                        0, 
+                                        NULL, 
+                                        &proc_ops_pyjama);
+
+    printk("ldd driver init exit");
+    return 0;
+}
+
+static void ldd_driver_module_exit(void)
+{
+    printk("ldd driver exit entry");
+    
+    proc_remove(proc_dir_entry_pyjama);
+
+    printk("ldd driver remove exit");
+}
+
+module_init(ldd_driver_module_init);
+module_exit(ldd_driver_module_exit);
